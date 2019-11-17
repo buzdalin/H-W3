8fef8b8e3a4d3b35a3c96d942553bb4c0c546b37
buzdalin <57385619+buzdalin@users.noreply.github.com>
2019-11-17 21:13:29 +0300

    Add files via upload
    Определился с сайтом, начал делать основную страницу без скрипта

---
+++ Home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="Home.css">
+	<meta charset="utf-8">
+</head>
+<body>
+<div class="exit"><p>Exit</p></div>
+	<div class="main">
+		<div class="menu">
+			<p id="Menu1">Menu</p>
+			<p>Home</p>
+			<p>My page</p>
+		</div>
+		<div>
+			<?php 
+			 ?>
+		</div>
+	</div>
+
+
+</body>
+</html>
