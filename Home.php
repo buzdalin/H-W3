c9d93b0b5217806e58ae3b874dbed1a47d7e5e8d
buzdalin <57385619+buzdalin@users.noreply.github.com>
2019-11-21 22:32:59 +0300

    Домашняя страница Home.php
    Изменил пункты меню
    Сделал стиль оглавления в строке

--- Home.php
+++ Home.php
@@ -2,24 +2,22 @@
 <html>
 <head>
 	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="Home.css">
+	<link rel="stylesheet" type="text/css" href="Css.css">
 	<meta charset="utf-8">
 </head>
 <body>
 <div class="exit"><p>Exit</p></div>
 	<div class="main">
 		<div class="menu">
-			<p id="Menu1">Menu</p>
+			<p style="margin: 0px; background-color:#8F4700; color: #BC7026; text-align: center; border: solid #013440 1.5px">
+			Navigation</p>
 			<p>Home</p>
-			<p>My page</p>
+			<p>My warehouses</p>
+			<p>Account</p>
 		</div>
 		<div>
 			<p>Text</p>
-			<?php 
-			 ?>
 		</div>
 	</div>
-
-
 </body>
 </html>
