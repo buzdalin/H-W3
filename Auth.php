de1836aaa64ac35fab14a6d366c9bad017c603d0
buzdalin <57385619+buzdalin@users.noreply.github.com>
2019-11-23 22:45:26 +0300

    Скрипт авторизации
    Разработал скрипт без сессии

--- Auth.php
+++ Auth.php
@@ -1,20 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Authorization</title>
-	<link rel="stylesheet" type="text/css" href="Css.css">
-	<meta charset="utf-8">
-</head>
-<body>
-	<div class="reg">
-	<h1>Authorization</h1>
-<form action="" method="post">
-<p><input type="text" name="login" placeholder="Login"><br></p>
-<p><input type="text" name="password" placeholder="Password"><br></p>
-<p><input type="checkbox" name="sudo">Head</p>
-	<input type="submit" value="Authorize">
-</form><br>
-<a href="">Registration</a>
-	</div>
-</body>
-</html>
+<?php 
+$login=$_POST['login'];
+$pass=$_POST['password'];
+
+$mysql = new mysqli('localhost', 'root','', 'auth');
+
+$result=$mysql->query("SELECT * FROM `users` WHERE `login`='$login' AND `pass`='$pass'");
+$user = $result->fetch_assoc();
+if (count($user)==0) {
+	echo "Invalid login or password";
+	exit();}
+$mysql->close();
+header('Location: Home.php');
+?>
