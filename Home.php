cd7627b38957513b70d72e39cec795f18bc9507c
buzdalin <57385619+buzdalin@users.noreply.github.com>
2019-11-24 19:17:26 +0300

    Домашняя страница
    Добавил проверку Куки
    Если Куки нет, пользователь переадрисовывается на страницу авторизации

--- Home.php
+++ Home.php
@@ -1,3 +1,5 @@
+<?php if ($_COOKIE['users']==''): header('location:Auth.html'); exit; ?>
+<?php else:?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,18 +8,22 @@
 	<meta charset="utf-8">
 </head>
 <body>
+	
 <div class="exit"><p>Exit</p></div>
 	<div class="main">
 		<div class="menu">
 			<p style="margin: 0px; background-color:#8F4700; color: #BC7026; text-align: center; border: solid #013440 1.5px">
 			Navigation</p>
-			<p>Home</p>
-			<p>My warehouses</p>
+			<p><a href="Home.php">Home</a></p>
+			<a href="Mywarehouses.php">My warehouses</a>
 			<p>Account</p>
 		</div>
 		<div>
 			<p>Text</p>
 		</div>
 	</div>
+
 </body>
 </html>
+
+<?php endif ?>
