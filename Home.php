7072168b998a6bf8c97b7621773eb6af1ca66958
buzdalin <57385619+buzdalin@users.noreply.github.com>
2019-11-25 23:41:31 +0300

    Скрипт проверки пользователя
    Переназначил файл под проверку пользователя и дальнейшей переадресации

--- Home.php
+++ Home.php
@@ -1,29 +1,4 @@
 <?php if ($_COOKIE['users']==''): header('location:Auth.html'); exit; ?>
-<?php else:?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="Css.css">
-	<meta charset="utf-8">
-</head>
-<body>
-	
-<div class="exit"><p>Exit</p></div>
-	<div class="main">
-		<div class="menu">
-			<p style="margin: 0px; background-color:#8F4700; color: #BC7026; text-align: center; border: solid #013440 1.5px">
-			Navigation</p>
-			<p><a href="Home.php">Home</a></p>
-			<a href="Mywarehouses.php">My warehouses</a>
-			<p>Account</p>
-		</div>
-		<div>
-			<p>Text</p>
-		</div>
-	</div>
-
-</body>
-</html>
-
-<?php endif ?>
+<?php elseif ($_COOKIE['users']=='user'): header('location: Home.html');exit;?>
+<?php elseif ($_COOKIE['users']=='sudo'): header('location: Homesudo.html');exit;?>
+<?php endif;?>
