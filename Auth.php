82e172873677934628d33b84ba6697927212fe05
buzdalin <57385619+buzdalin@users.noreply.github.com>
2019-12-01 22:28:43 +0300

    Скрипт авторизации
    Авторизация с зашифрованным паролем

--- Auth.php
+++ Auth.php
@@ -4,12 +4,19 @@ $pass=$_POST['password'];
 
 $mysql = new mysqli('localhost', 'root','', 'auth');
 
-$result=$mysql->query("SELECT * FROM `users` WHERE `login`='$login' AND `pass`='$pass'");
+$result=$mysql->query("SELECT `login`, `pass`, `sudo` FROM `users` WHERE `login`='$login'");
 $user = $result->fetch_assoc();
-if (count($user)==0) {
-	echo "Invalid login or password";
-	exit();}
-setcookie('users', $user['login'], time()+180, "/");
+if (empty($user)) {
+	echo "Invalid login";
+	exit();
+}
+if (!(password_verify($pass, $user['pass']))) {
+	echo "Invalid password";
+	exit();
+}
+
+setcookie('users', $user['sudo'], time()+180, "/");
+setcookie('login', $user['login'], time()+180, "/");
 $mysql->close();
-header('Location: Home.php');
+header('Location: Home.php')
 ?>
