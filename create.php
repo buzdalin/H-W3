2b738e052ea123bb1a096cfc635435fbe02351b3
buzdalin <57385619+buzdalin@users.noreply.github.com>
2019-12-02 20:35:07 +0300

    Скрипт создания Нового склада
    Скрипт создает Строчку в таблице в БД( с уже имеющейся таблицей с логинами пользователей) для всех складов и создает Новая таблица в новой БД с таблицами для всех существующих складов, которая называется уникальным кодом для каждого склада.

--- create.php
+++ create.php
@@ -1,24 +1,37 @@
 <?php
+if (empty($_COOKIE['login'])) {
+	header("location: Auth.html");
+	exit();}
 $name=$_POST['name'];
+$cookie=$_COOKIE['login'];
 
-$mysql = new mysqli('localhost', 'root','');
-$mysql->query("CREATE DATABASE IF NOT EXISTS `$name`");
-$mysql->query("CREATE TABLE IF NOT EXISTS `$name`. `$name` ( `id` INT(4) NOT NULL AUTO_INCREMENT , `text` VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL , UNIQUE `id` (`id`)) ENGINE = MyISAM CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;");
+$code=random_bytes(8);
+$code=bin2hex($code);
 
+$mysql = new mysqli('localhost', 'root','', 'auth');
 
+	$result=$mysql->query("SELECT `code` FROM `list`");
+		while ($dbcode = $result->fetch_assoc()) {
+			if ($dbcode['code']==$code) {
+		
+				$code=random_bytes(8);
+				$code=bin2hex($code);};};
 
+$mysql->query("INSERT INTO `list` (`login`,`text`, `code`) VALUES ('$cookie','$name', '$code');");
+
+$mysql->close();
+$mysql = new mysqli('localhost', 'root','','warehouses');
+
+$mysql->query("CREATE TABLE IF NOT EXISTS `$code` ( `id` INT(4) NOT NULL AUTO_INCREMENT , `text` VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL , UNIQUE `id` (`id`)) ENGINE = MyISAM CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;");
+
+$mysql->query("INSERT INTO `$code` (`text`) VALUES ('$name');");
 
 
 
 
 $mysql->close();
 
-$mysql = new mysqli('localhost', 'root','');
-$mysql->query("INSERT INTO `$name` (`text`) VALUES ('$name');");
-
-$mysql->close();
-
-
+header("location: create.html")
 
 
  ?>
