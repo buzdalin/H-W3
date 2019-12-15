3cf2040a762ba42867448c93cb07beb4219d18c1
buzdalin <57385619+buzdalin@users.noreply.github.com>
2019-12-16 01:05:10 +0300

    Update and rename Home.php to Dz/Site/UniScripts/Home.php
    Скрипт проверки пользователей

--- Home.php
+++
@@ -1,4 +0,0 @@
-<?php if ($_COOKIE['users']==''): header('location:Auth.html'); exit; ?>
-<?php elseif ($_COOKIE['users']=='user'): header('location: Home.html');exit;?>
-<?php elseif ($_COOKIE['users']=='sudo'): header('location: Homesudo.html');exit;?>
-<?php endif;?>
