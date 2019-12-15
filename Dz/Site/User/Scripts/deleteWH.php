19116b411e52be51cd5bad6c967e70cd069b333f
buzdalin <57385619+buzdalin@users.noreply.github.com>
2019-12-16 01:16:27 +0300

    Update and rename deleteWH.php to Dz/Site/User/Scripts/deleteWH.php
    Скрипт удаления склада

--- Dz/Site/User/Scripts/deleteWH.php
+++ Dz/Site/User/Scripts/deleteWH.php
@@ -1,6 +1,6 @@
 <?php
 if (empty($_COOKIE['login'])) {
-	header("location: Auth.html");
+	header("location: \Dz\Site\Uni\Auth.html");
 	exit();};
 $login=$_COOKIE['login'];
 $code=$_GET['code'];
@@ -8,5 +8,5 @@ $mysql = new mysqli('localhost', 'root','', 'auth');
 $result=$mysql->query("DELETE FROM `list` WHERE `code`='$code'");
 $result=$mysql->query("DROP TABLE `auth`.`$code`");
 $mysql->close();
-header('location: Mywarehouses.php')
+header('location: \Dz\Site\User\Mywarehouses.php')
 ?>
