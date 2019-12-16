e54556837d670712927d47a2e412d31c7232e1aa
buzdalin <57385619+buzdalin@users.noreply.github.com>
2019-12-16 21:38:49 +0300

    Update Home.php

--- Dz/Site/UniScripts/Home.php
+++ Dz/Site/UniScripts/Home.php
@@ -1,5 +1,5 @@
 <?php 
-if ($_COOKIE['users']==''){ header("location:/Dz/site/Uni/Auth.html"); exit();} 
+if ($_COOKIE['users']==''){ header("location:/Dz/site/Uni/Unauth.php"); exit();} 
  elseif ($_COOKIE['users']=="user"){ header("location: /Dz/site/User/HomeU.php");exit();}
  	elseif ($_COOKIE['users']=="sudo"){ header("location: /Dz/site/Sudo/Homesudo.php");exit();}
 ?>
