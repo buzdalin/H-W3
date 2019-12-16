bc3c058a19a067dc77f4d2bad0095e37953c5a33
buzdalin <57385619+buzdalin@users.noreply.github.com>
2019-12-16 21:39:44 +0300

    Create Unauth.php

---
+++ Dz/Site/Uni/Unauth.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Warehouses</title>
+	<link rel="stylesheet" type="text/css" href="/Dz/site/Css.css">
+	<meta charset="utf-8">
+</head>
+<body>
+	
+<div class="header">
+		<h1>Warehouses</h1>
+</div>
+<div class="main">
+	<div class="content">
+		<h1 style="text-align: center;">Hello!</h1>
+		<p>Welcome unauthorised user!</p>
+		<p>On this wed-site you can create and manage your own Warehouse</p>
+		<a href="/Dz/site/Uni/Auth.html">Authorise</a> or <a href="/Dz/site/Uni/Reg.html">Registrate</a>
+	</div></div>
+<div class="footer">
+		<p>Выполнено студентом МГТУ им. Н.Э. Баумана</p>
+			<p>ИУ4-11Б</p>
+</div>
+</body>
+</html>
